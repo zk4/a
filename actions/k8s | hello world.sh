f0b6d113efb6242ded15b5b033dc10a7fdef83b6
zk <liuzq7@gmail.com>
2022-10-07 19:32:13 +0800

    chore: k8s

--- actions/k8s | hello world.sh
+++ actions/k8s | hello world.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
-
 set -x 
+
 kubectl run hello --image=googlecontainer/echoserver --port=8082
-
-
-
-var=`fzf_input "description" ls`
 
--- actions/k8s | nuke all.sh
+++ actions/k8s | nuke all.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-input var_name "var description" 
-var_name="${var_name/#\~/$HOME}"
+source "$(dirname "$0")/../util/base.sh"
+
+color_input var_name "确定要 nuke 掉所有？（yes|no)" 
+
+if [[ -n $var_name && $var_name == "yes" ]]; then
+  kubectl delete deploy --all
+  kubectl delete svc --all
+  kubectl delete pod --all
+fi
 
 
-kubectl delete deploy --all
-kubectl delete svc --all
