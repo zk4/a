0bf6b5af97b8b1f0ed03ad837272bb1b067825af
zk <liuzq7@gmail.com>
2022-12-01 15:35:06 +0800

    chore: add action

---
+++ actions/prj | yonwise member.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+source "$(dirname "$0")/../util/base.sh"
+
+
+
+open 'file:///Users/zk/Library/Mobile%20Documents/iCloud~md~obsidian/Documents/%E5%B2%B3%E6%85%A7%E4%BA%913.0-P0/index.html'
+
+idea '/Users/zk/git/yonwise/nxcloud-app-member'
+
+idea '/Users/zk/git/yonwise/nxcloud-middle-member'
