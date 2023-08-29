ce80fa2577f06101bbea8a21736d8796ee9abda0
zk <liuzq7@gmail.com>
2023-08-29 11:44:42 +0800

    u

---
+++ actions/bash | create autosrc.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+source "$(dirname "$0")/../util/base.sh"
+
+
+color_input pversion "python version(ex:3.10.12):"
+
+# Text to write to .autosrc
+autosrc_text=$(cat <<EOF
+# Called when you first enter the directory (or its children)
+autosrc_enter() {
+
+	#replace pwd / to _
+	environment_name=$(echo "$PWD" | sed 's/\//_/g')-$pversion
+
+	if conda info --envs | grep -q "\\\b\$environment_name\\\b"; then
+	    echo ""
+	else
+	    conda create -y -n "\$environment_name" python=$pversion
+	fi
+
+	conda activate \$environment_name
+  	echo "activate"
+}
+
+autosrc_exit() {
+	conda deactivate
+}
+EOF
+)
+
+# Write the text to .autosrc
+echo "$autosrc_text" > ./.autosrc
+
+echo "Text written to .autosrc"
